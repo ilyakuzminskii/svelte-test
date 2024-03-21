2892e7bb3bf86c6446202d642bb515baf2ec6287
kuzmi <kuzminskyilya@yandex.ru>
2024-03-21 17:11:32 +0300

    add main page and nav

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<svelte:head>
+<title>Илья Кузьминский</title>
+</svelte:head>
+
+<div class='container'>
+    <div class='info'>
+        <div class='animate-background'>
+            <div class='container info-container'>
+                <div class='info-text'>
+                    <h1>
+                        <p class='animate-text-1'>Lorem ipsum dolor sit,</p>
+                        <p class='text-2'>Lorem ipsum dolor sit amet.</p>
+                    </h1>
+                </div>
+                <h2 class='subtitle'>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium architecto aut,
+cupiditate dolores
+ducimus expedita labore nemo odio quibusdam quidem sapiente soluta.
+                </h2>
+                <div style='color:black' class='scroll-button animate-scroll' tabindex='0'>
+                    <a href='/about'>Click Here</a>
+                    <div class='arrow'></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class='background-div'></div>
